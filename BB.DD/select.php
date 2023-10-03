4608d13e8d45733c218a4f2fd42021fce2061f3f
lauraAltava <laumaltava@gmail.com>
2023-10-03 11:20:22 +0200

    decimo dia

--- BB.DD/select.php
+++ BB.DD/select.php
@@ -2,6 +2,27 @@
 require('Producto.php');
 
 require('conexion.php');
+$precio = $_GET['id'] ?? 1;
 
-$resultado = $pdo->query("SELECT producto, unidades FROM stock WHERE id = " . $id);
+$pdoSt= $pdo->query("SELECT nombre,codigodebarras,precio from producto WHERE id = ?");
+$pdoSt->bindParam(1, $id);
+$pdoSt->execute();
+
+$producto = $pdoSt->fetch();
+
+echo "Nombre: " . $producto['nombre'] . "<br>";
+echo "Unidades: " . $producto['unidades'] . "<br>";
+echo "Precio: " . $producto['precio'] . "<br>";
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
